302906ba852937eafa8da042ad438011f765e2bb
longfei <lxxgfeis.3@gmail.com>
2021-03-26 00:58:13 +0800

    make shfmt happy again

--- golang/install.sh
+++ golang/install.sh
@@ -2,7 +2,7 @@
 if test ! "$(which go)"; then
   if test "$(which brew)"; then
     echo "  Installing Golang for you."
-    brew install go > /dev/null 2>&1
+    brew install go >/dev/null 2>&1
   fi
 fi
 
