42b89452aadaa6b805dd174e536937565287ce28
longfei <lxxgfeis.3@gmail.com>
2021-03-26 09:21:55 +0800

    add powerlevel 10k

---
+++ p10k/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if test "$(which brew)"; then
+  if [ ! -d "$(brew --prefix)"/opt/powerlevel10k ]; then
+    echo "  Installing Powerlevel10k for you."
+    brew install romkatv/powerlevel10k/powerlevel10k >/dev/null 2>&1
+  fi
+fi
+
+exit 0
