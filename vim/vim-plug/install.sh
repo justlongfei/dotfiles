2dbad4572221618c21358d728514516e71ff4274
longfei <lf.x.0720@gmail.com>
2021-04-08 09:12:17 +0800

    [style][2021-04-08 09:12] fix ci

--- vim/vim-plug/install.sh
+++ vim/vim-plug/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ ! -f "$HOME"/.vim/autoload/plug.vim ]; then
-    echo "› Installing vim-plug for you."
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  echo "› Installing vim-plug for you."
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
