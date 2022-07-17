00f0f7280d821a9b4a9f4475addc15fc86e9ebb2
longfei <longfei1bot@gmail.com>
2022-07-17 14:09:44 +0800

    update 2022-07-17

--- nodejs/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if test "$(which brew)"; then
-  if test ! "$(which node)"; then
-    echo "  Installing nodejs for you."
-    brew install node >/dev/null 2>&1
-  fi
-fi
-
-exit 0
--- system/aliases.zsh
+++ system/aliases.zsh
@@ -19,7 +19,7 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 
 alias r!="echo 'reload zsh config' && source ~/.zshrc"
-alias cls='clear' # Good 'ol Clear Screen command
+alias clr='clear' # Good 'ol Clear Screen command
 alias pubkey="more ~/.ssh/id_ed25519.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 alias pretty_date="date '+%Y-%m-%d'"
 alias grep="grep --color=auto"
